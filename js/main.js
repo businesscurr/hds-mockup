66ff7ac6cbdae10ff01206e76e80065810f9512a
Eduardo Quiñonez <eduardo.grquinonez@gmail.com>
2021-10-11 22:10:41 -0500

    Se adapta para desktop sección de FAQ page

--- js/main.js
+++ js/main.js
@@ -3,5 +3,17 @@
 const toggleQuestion = (question) => {
     let pregunta = `pregunta${question}`
     let result = document.getElementById(pregunta)
+    let preguntaContainer = document.getElementById(`question${question}`)
+
+    // Función para determinar el álto máximo a los contenedores de las preguntas.
+    function addHeight(){
+        let questions = document.querySelectorAll(".question")
+        questions.forEach(element => {
+            element.classList.add('maxHeight')
+        });
+        preguntaContainer.classList.remove("maxHeight")
+
+    }
+    addHeight()    
     result.classList.toggle("d-none")
 }
